4b6828c0b250abc9231409f68f03d4a861a06929
nikux <nicolasbertolo@gmail.com>
2014-04-06 01:16:36 -0300

    More member functions of component are now public. Also added try versions of
    existing member functions and added one to return the subsystem that accepted the
    component.

--- lib/include/component.h
+++ lib/include/component.h
@@ -19,7 +19,7 @@ namespace game_engine
 
 			/** \brief Stores a pointer to the entity that owns this component. */
 			entity* parent_ent = nullptr;
-		protected:
+		public:
 			/** \brief Returns a reference to the game that owns the entity that owns
 			 * this component. If there isn't one, it throws std::runtime_error.
 			 */
@@ -29,7 +29,27 @@ namespace game_engine
 			 * component. Throws std::runtime_error if there isn't any.
 			 */
 			entity& get_parent_ent() const;
-		public:
+
+			/** \brief Returns a reference to the subsystem that accepted this
+			 * component. Throws std::runtime_error if there isn't any.
+			 */
+			subsystem& get_subsys() const;
+
+			/** \brief Tries to return the parent game.
+			 * Returns nullptr if there isn't any or this component isn't owned by any entity.
+			 */
+			game*  try_get_parent_game() const;
+
+			/** \brief Tries to return the parent game.
+			* Returns nullptr if there isn't any.
+			*/
+			entity*  try_get_parent_entity() const;
+
+			/** \brief Tries to return the subsystem that accepted this component.
+			* Returns nullptr if there isn't any.
+			*/
+			subsystem*  try_get_subsys() const;
+
 			virtual ~component();
 		};
 	}
--- lib/src/component.cpp
+++ lib/src/component.cpp
@@ -21,6 +21,30 @@ namespace game_engine
 			return get_parent_ent().get_parent_game();
 		}
 
+		subsystem& component::get_subsys() const
+		{
+			if (subsys)
+			{
+				return *subsys;
+			}
+			throw std::runtime_error("Component not accepted by any entity.");
+		}
+
+		entity* component::try_get_parent_entity() const
+		{
+			return parent_ent;
+		}
+
+		game* component::try_get_parent_game() const
+		{
+			return parent_ent ? parent_ent->parent_game : nullptr;
+		}
+
+		subsystem* component::try_get_subsys() const
+		{
+			return subsys;
+		}
+
 		component::~component()
 		{
 			if (subsys)
