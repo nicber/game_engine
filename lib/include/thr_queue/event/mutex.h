5ed2217b540308d149c78351e65d266bfbb07e10
Nicolas Bertolo <nicolasbertolo@gmail.com>
2014-06-17 11:35:13 -0300

    Added a mutex type that takes into account coroutines. I tried to implement the Mutex concept so it can be used with lock_guard, unique_lock and anything that works with Mutexes.

---
+++ lib/include/thr_queue/event/mutex.h
@@ -0,0 +1,22 @@
+#include <atomic>
+#include <mutex>
+
+namespace game_engine {
+namespace thr_queue {
+class coroutine;
+
+namespace event {
+class mutex {
+public:
+  void lock();
+  bool try_lock();
+  void unlock();
+
+private:
+  std::mutex mt;
+  coroutine *owning_cor;
+  std::deque<coroutine> waiting_cors;
+};
+}
+}
+}
---
+++ lib/src/thr_queue/event/mutex.cpp
@@ -0,0 +1,56 @@
+#include "../global_thr_pool_impl.h"
+#include "thr_queue/event/lock.h"
+
+namespace game_engine {
+namespace thr_queue {
+namespace event {
+void mutex::lock() {
+  std::unique_lock<std::mutex> lock(mt, std::defer_lock);
+
+  // we might have been scheduled by a thread that unlocked the lock, but that
+  // doesn't guarantee that another thread didn't lock this lock before we had
+  // time to run.
+  while (true) {
+    assert(!lock.owns_lock());
+    lock.lock();
+
+    if (!owning_cor) {
+      owning_cor = running_coroutine_or_yielded_from;
+      return;
+    }
+
+    global_thr_pool.yield([&] {
+      std::unique_lock<std::mutex> lock_lamb(mt, std::adopt_lock);
+      waiting_cors.emplace_back(std::move(*running_coroutine_or_yielded_from));
+    });
+  }
+}
+
+bool mutex::try_lock() {
+  std::unique_lock<std::mutex> lock(mt);
+
+  if (owning_cor) {
+    return false;
+  } else {
+    owning_cor = running_coroutine_or_yielded_from;
+  }
+
+  return true;
+}
+
+void mutex::unlock() {
+  std::unique_lock<std::mutex> lock(mt);
+
+  assert(running_coroutine_or_yielded_from == owning_cor);
+
+  owning_cor = nullptr;
+
+  if (waiting_cors.size()) {
+    auto cor = std::move(waiting_cors.front());
+    waiting_cors.pop_front();
+    global_thr_pool.schedule(std::move(cor), true);
+  }
+}
+}
+}
+}
