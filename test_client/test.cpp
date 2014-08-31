221560fc7106ee2059b84082de1355cf16144622
Nicolas Bertolo <nicolasbertolo@gmail.com>
2014-08-31 19:09:57 -0300

    Added a small test client.

---
+++ test_client/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "launcher_api.h"
+#include "world.h"
+
+void GAME_EXECUTION_FUNCTION (game_engine::logic::game *gam) {
+  std::cout << "lala\n";
+}	
+
+int main() {
+	
+}
