f442f3e8e28f04d6dbc7135a9948c52a4ae6d84c
Nicolas Bertolo <nicolasbertolo@gmail.com>
2014-09-16 19:50:37 -0300

    Worked more on the test client.

--- test_client/test.cpp
+++ test_client/test.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include "launcher_api.h"
+#include "subsystems/render/render.h"
 #include "world.h"
 
 void GAME_EXECUTION_FUNCTION (game_engine::logic::game *gam) {
-  while(true) {
+  using namespace game_engine::logic;
+  using namespace game_engine::logic::subsystems::render;
+  
+  render_subsystem *rend_sub = nullptr;
+  
+  try {
+    rend_sub = &gam->get<render_subsystem>();
+  } catch (game::no_subsystem_found<render_subsystem> &) {
+    std::cerr << "No rendering subsystem found.\n"
+      "WARNING: There's no way to know that the game should stop,\n so it will"
+      "         run indefinitely.";
+  }
+  
+  
+  while(!rend_sub || !rend_sub->has_exited()) {
     gam->tick();
   }
 }	
-
-int main() {
-	
-}
