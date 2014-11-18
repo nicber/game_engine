4a56d079263769f321595198fbf3e688154fca9e
Nicolas Bertolo <nicolasbertolo@gmail.com>
2014-11-18 17:17:43 -0300

    Fixed a small bug that appeared when the uv thread didn't have time to initialize.
    We were using a uv_async as if it had been correctly initialized, but
    that didn't necessarily happen before the destruction of the uv_thread.

--- lib/src/thr_queue/event/uv_thread.cpp
+++ lib/src/thr_queue/event/uv_thread.cpp
@@ -18,12 +18,21 @@ public:
       , thr(run_loop) {}
 
   ~uv_thread() {
+    // we have to make sure that we call uv_async_send with an async handler that's
+    // already initialized.
     should_stop = true;
+    boost::unique_lock<boost::mutex> lock(init_start_requests_mt, boost::defer_lock);
+    while (lock.lock(), !async_constructed) {
+      lock.unlock();
+    }
+    assert (async_constructed);
     uv_async_send(global_async.get());
+    lock.unlock();
     thr.join();
   }
 
   std::atomic<bool> should_stop;
+  bool async_constructed = false;
   std::unique_ptr<uv_async_t> global_async = nullptr;
   boost::thread thr;
   std::deque<std::function<void()>> init_start_requests;
@@ -34,6 +43,7 @@ static uv_thread thr;
 
 void uv_thr_init(uv_async_t *async, uv_async_cb f_ptr) {
   assert(thr.global_async);
+  assert(!thr.should_stop);
 
   boost::mutex mt;
   boost::condition_variable cv;
@@ -58,11 +68,6 @@ void uv_thr_init(uv_async_t *async, uv_async_cb f_ptr) {
 }
 
 void process_init_start_requests_or_stop(uv_async_t *) {
-  if (thr.should_stop) {
-    uv_stop(uv_default_loop());
-    return;
-  }
-
   boost::unique_lock<boost::mutex> lock(thr.init_start_requests_mt,
                                     boost::defer_lock);
 
@@ -73,13 +78,21 @@ void process_init_start_requests_or_stop(uv_async_t *) {
       f();
     }
   }
+
+  if (thr.should_stop) {
+    uv_stop(uv_default_loop());
+    return;
+  }
 }
 
 void run_loop() {
   assert(thr.global_async);
+  boost::unique_lock<boost::mutex> lock(thr.init_start_requests_mt);
   uv_async_init(uv_default_loop(),
                 thr.global_async.get(),
                 process_init_start_requests_or_stop);
+  thr.async_constructed = true;
+  lock.unlock();
 
   uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 }
