190b0dc410641794e58f78ee569c8a652bf95c05
Nicolas Bertolo <nicolasbertolo@gmail.com>
2015-01-12 11:28:17 -0300

    Fixed a bug related to data being initialized but then default-constructed thus erasing it.

--- lib/include/opengl/uniform_buffer.h
+++ lib/include/opengl/uniform_buffer.h
@@ -16,18 +16,25 @@ bool uniform_buff_variable_sort_func(const uniform &lhs, const uniform &rhs);
 /** \brief Compares a uniform and a string that stores a uniform name. */
 bool uniform_buff_variable_sort_with_name_func(const uniform &lhs, const std::string &rhs);
 
+/** \brief Utility for constructing the buffer base class and the data member at the same time. */
+struct buffer_and_data_constructor {
+  buffer_and_data_constructor(const program &prog, const std::string &name);
+  std::shared_ptr<uniform_buffer_data> data;
+  size_t block_size;
+};
+
 /** \brief Subclass of buffer<unsigned char> that implements a uniform buffer.
  * It implements utility functions for setting a specific variable of the
  * uniform block stored by this function.
  */
-class uniform_buffer : public buffer<unsigned char> {
+class uniform_buffer : buffer_and_data_constructor, public buffer<unsigned char> {
 public:
   /** \brief Constructs a uniform buffer that stores a uniform block compatible
    * with the one named 'block_name' of the program 'prog'.
    * 'freq_acc' and 'kind_acc' specify buffer specific options. See buffer<T> for
    * more information.
    */
-  uniform_buffer(const program &prog, std::string block_name,
+  uniform_buffer(const program &prog, const std::string &block_name,
                  buf_freq_access freq_acc, buf_kind_access kind_acc);
 
   /** \brief Returns an iterator to the start of the variable identified by name. */
@@ -36,13 +43,7 @@ public:
   const_iterator cbegin(const std::string &name) const;
 
   void bind_to(uniform_block_binding_handle handle);
-private:
   /** \brief Utility member function for constructing the buffer base class. */
-  buffer<unsigned char> construct_buffer_and_variables(const program &prog, const std::string &block_name,
-                                         buf_freq_access freq_acc, buf_kind_access kind_acc);
-
-private:
-  std::shared_ptr<uniform_buffer_data> data;
 };
 
 }
--- lib/src/opengl/uniform_buffer.cpp
+++ lib/src/opengl/uniform_buffer.cpp
@@ -12,9 +12,35 @@ bool uniform_buff_variable_sort_with_name_func(const uniform &lhs, const std::st
   return lhs.name < rhs;
 }
 
-uniform_buffer::uniform_buffer(const program &prog, std::string block_name,
+buffer_and_data_constructor::buffer_and_data_constructor(const program &prog, const std::string &name)
+{
+  GLint uniform_block_index = -1;
+  const auto &unifs = prog.get_uniforms();
+  data = std::make_shared<uniform_buffer_data>();
+
+  for (auto it = unifs.get<0>().cbegin(); it != unifs.get<0>().cend(); ++it) {
+    if (uniform_block_index != -1 && it->block_index == uniform_block_index) {
+      data->variables.emplace_back(*it);
+      assert(block_size == (size_t)it->block_size);
+      continue;
+    }
+
+    if (it->block_name == name) {
+      uniform_block_index = it->block_index;
+      block_size = it->block_size;
+      data->variables.emplace_back(*it);
+    }
+  }
+
+  if (uniform_block_index == -1) {
+    throw std::runtime_error("no uniform block with name: " + name);
+  }
+}
+
+uniform_buffer::uniform_buffer(const program &prog, const std::string &block_name,
                                buf_freq_access freq_acc, buf_kind_access kind_acc):
-  buffer<unsigned char>(construct_buffer_and_variables(prog, block_name, freq_acc, kind_acc))
+  buffer_and_data_constructor(prog, block_name),
+  buffer<unsigned char>(block_size, freq_acc, kind_acc)
 {
   assert(data->variables.size() && "there can't be a uniform block with 0 variables");
   std::sort(data->variables.begin(), data->variables.end(), uniform_buff_variable_sort_func);
@@ -47,36 +73,6 @@ buffer<unsigned char>::const_iterator uniform_buffer::cbegin(const std::string &
 void uniform_buffer::bind_to(uniform_block_binding_handle handle) {
   handle->bound_buffer_data = data;
   glBindBufferBase(GL_UNIFORM_BUFFER, handle->id, get_buffer_id());
-}
-
-buffer<unsigned char> uniform_buffer::construct_buffer_and_variables(
-                                      const program &prog, const std::string &name,
-                                      buf_freq_access freq_acc,
-                                      buf_kind_access kind_acc)
-{
-  GLint uniform_block_index = -1;
-  GLint block_size;
-  const auto &unifs = prog.get_uniforms();
-  data = std::make_shared<uniform_buffer_data>();
-
-  for (auto it = unifs.get<0>().cbegin(); it != unifs.get<0>().cend(); ++it) {
-    if (uniform_block_index != -1 && it->block_index == uniform_block_index) {
-      data->variables.emplace_back(*it);
-      assert(block_size == it->block_size);
-      continue;
-    }
-
-    if (it->block_name == name) {
-      uniform_block_index = it->block_index;
-      block_size = it->block_size;
-    }
-  }
-
-  if (uniform_block_index == -1) {
-    throw std::runtime_error("no uniform block with name: " + name);
-  }
-
-  return {(size_t)block_size, freq_acc, kind_acc};
 }
 }
 }
