0706c604f921b48bb0ec589a0729f5a58e1ab3e4
Nicolas Bertolo <nicolasbertolo@gmail.com>
2015-01-23 21:19:18 -0300

    Made meshes aware of uniform buffers.

--- lib/include/render/mesh.h
+++ lib/include/render/mesh.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include "opengl/program.h"
+#include "opengl/uniform_buffer.h"
 #include "opengl/vertex_array_object.h"
 #include <vector>
 
@@ -9,8 +10,11 @@ namespace game_engine {
 namespace render {
 class mesh {
 public:
-  mesh(std::shared_ptr<opengl::program> prog_,
-       std::shared_ptr<opengl::vertex_array_object> vao_,
+  using uni_buff_vector = std::vector<std::shared_ptr<const opengl::uniform_buffer>>;
+public:
+  mesh(std::shared_ptr<const opengl::program> prog_,
+       std::shared_ptr<const opengl::vertex_array_object> vao_,
+       uni_buff_vector uni_buffs_,
        size_t count_,
        size_t base_index_,
        size_t base_vertex_,
@@ -21,13 +25,14 @@ public:
 
   void draw(unsigned long long delta_time) const;
 private:
-  std::shared_ptr<opengl::program> prog;
-  std::shared_ptr<opengl::vertex_array_object> vao;
-  size_t count;
-  size_t base_index;
-  size_t base_vertex;
-  size_t instance_count;
-  size_t base_instance;
+  std::shared_ptr<const opengl::program> prog;
+  std::shared_ptr<const opengl::vertex_array_object> vao;
+  uni_buff_vector uni_buffs;
+  const size_t count;
+  const size_t base_index;
+  const size_t base_vertex;
+  const size_t instance_count;
+  const size_t base_instance;
 };
 }
 }
--- lib/src/render/mesh.cpp
+++ lib/src/render/mesh.cpp
@@ -2,8 +2,9 @@
 
 namespace game_engine {
 namespace render {
-mesh::mesh(std::shared_ptr<opengl::program> prog_,
-           std::shared_ptr<opengl::vertex_array_object> vao_,
+mesh::mesh(std::shared_ptr<const opengl::program> prog_,
+           std::shared_ptr<const opengl::vertex_array_object> vao_,
+           std::vector<std::shared_ptr<const opengl::uniform_buffer>> uni_buffs_,
            size_t count_,
            size_t base_index_,
            size_t base_vertex_,
@@ -11,6 +12,7 @@ mesh::mesh(std::shared_ptr<opengl::program> prog_,
            size_t base_instance_)
  :prog(std::move(prog_)),
   vao(std::move(vao_)),
+  uni_buffs(std::move(uni_buffs_)),
   count(count_),
   base_index(base_index_),
   base_vertex(base_vertex_),
@@ -31,6 +33,14 @@ bool mesh::operator<(const mesh &rhs) const {
 void mesh::draw(unsigned long long delta_time) const {
   vao->bind();
   prog->bind();
+
+  for (const auto &ptr : uni_buffs) {
+    ptr->rebind();
+  }
+
+  if (!prog->ubb_manager().check_compatibility()) {
+    throw std::runtime_error("incompatible program and uniform buffer");
+  }
 
   auto index = reinterpret_cast<const void*>(base_index);
   glDrawElementsInstancedBaseVertexBaseInstance(GL_TRIANGLES, // mode
