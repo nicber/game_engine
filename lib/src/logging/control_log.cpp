af4e88740311349aee6366ef381eb1ad5710d047
Nicolas Bertolo <nicolasbertolo@gmail.com>
2015-06-27 19:31:45 -0300

    Switched control_log to recursive mutexes.

--- lib/src/logging/control_log.cpp
+++ lib/src/logging/control_log.cpp
@@ -1,7 +1,7 @@
 #include <atomic>
 #include <boost/functional/hash.hpp>
 #include <boost/thread/lock_guard.hpp>
-#include <boost/thread/mutex.hpp>
+#include <boost/thread/recursive_mutex.hpp>
 #include "logging/control_log.h"
 #include <unordered_map>
 
@@ -17,7 +17,7 @@ static std::atomic<bool> iterating(false);
 static policy default_policy = policy::enable;
 using line_map = std::unordered_map<unsigned int, policy>;
 static std::unordered_map<std::string, line_map> file_line_policies;
-static boost::mutex policy_mutex;
+static boost::recursive_mutex policy_mutex;
 
 static void check_not_iterating()
 {
@@ -34,15 +34,15 @@ void set_default_for_all(policy pol)
 void set_default_for_file(std::string file, policy pol)
 {
   check_not_iterating();
-  boost::lock_guard<boost::mutex> l(policy_mutex);
+  boost::lock_guard<boost::recursive_mutex> l(policy_mutex);
   file_line_policies[file][0] = pol;
 }
 
 void set_policy_for_file_line(std::string file, unsigned int line,
                               policy pol)
 {
-  boost::lock_guard<boost::mutex> l(policy_mutex);
   check_not_iterating();
+  boost::lock_guard<boost::recursive_mutex> l(policy_mutex);
   file_line_policies[file][line] = pol;
 }
 
@@ -50,7 +50,7 @@ void set_policy_for_file_line(std::string file, unsigned int line,
 policy get_policy_for(const std::string &file, unsigned int line)
 {
   check_not_iterating();
-  boost::lock_guard<boost::mutex> l(policy_mutex);
+  boost::lock_guard<boost::recursive_mutex> l(policy_mutex);
   auto line_map_it = file_line_policies.find(file);
   if (line_map_it != file_line_policies.end()) {
     auto &line_map = line_map_it->second;
@@ -69,7 +69,7 @@ policy get_policy_for(const std::string &file, unsigned int line)
 
 policy get_policy_for_file(const std::string &file)
 {
-  boost::lock_guard<boost::mutex> l(policy_mutex);
+  boost::lock_guard<boost::recursive_mutex> l(policy_mutex);
   auto line_map_it = file_line_policies.find(file);
   if (line_map_it != file_line_policies.end()) {
     auto &line_map = line_map_it->second;
@@ -84,7 +84,7 @@ policy get_policy_for_file(const std::string &file)
 
 policy get_default_policy()
 {
-  boost::lock_guard<boost::mutex> l(policy_mutex);
+  boost::lock_guard<boost::recursive_mutex> l(policy_mutex);
   return default_policy;
 }
 
@@ -96,7 +96,7 @@ void remove_file_policy(const std::string &file)
 void remove_file_line_policy(const std::string &file, unsigned int line)
 {
   check_not_iterating();
-  boost::lock_guard<boost::mutex> l(policy_mutex);
+  boost::lock_guard<boost::recursive_mutex> l(policy_mutex);
   auto line_map_it = file_line_policies.find(file);
   if (line_map_it != file_line_policies.end()) {
     auto &line_map = line_map_it->second;
@@ -124,7 +124,7 @@ bool operator!=(const applied_policy &lhs, const applied_policy &rhs)
 
 void apply_to_all_policies(visitor_func vf)
 {
-  boost::lock_guard<boost::mutex> l(policy_mutex);
+  boost::lock_guard<boost::recursive_mutex> l(policy_mutex);
   iterating = true;
   try {
     for(auto &p1 : file_line_policies) {
