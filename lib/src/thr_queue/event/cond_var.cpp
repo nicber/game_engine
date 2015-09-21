b708c0ab2309a8d9edb04ede56f58b268085a8a3
Nicolás Luciano Bértolo <nicolasbertolo@gmail.com>
2015-09-21 17:59:36 -0300

    Fixed a sneaky bug related to condition_variable.

--- lib/src/thr_queue/event/cond_var.cpp
+++ lib/src/thr_queue/event/cond_var.cpp
@@ -16,11 +16,18 @@ void condition_variable::wait(boost::unique_lock<mutex> &lock) {
   global_thr_pool.yield([&] {
     // running_coroutine_or_yielded from still points to this coroutine even
     // after having yielded.
-    lock_unlocker<boost::unique_lock<mutex>> l_unlock_co_mt(lock);
     lock_unlocker<boost::unique_lock<boost::mutex>> l_unlock_std_mt(lock_std);
+    lock_unlocker<boost::unique_lock<mutex>> l_unlock_co_mt(lock);
 
     waiting_cors.emplace_back(std::move(*running_coroutine_or_yielded_from));
   });
+
+  // If this coroutine is resumed by a thread different from the one that yielded,
+  // then it is possible that the changes to internal state of the 'lock' variable will
+  // not have been yet acknowledged by the resuming thread. 
+  // This special case will lead to a difficult to debug race condition involving
+  // the owns_lock member variable.
+  boost::lock_guard<boost::mutex> hack(mt);
   lock.lock();
 }
 
