53cff3dd6221abb805a2cb3568e5f5219ed1750c
Nicolás Luciano Bértolo <nicolasbertolo@gmail.com>
2015-09-21 18:04:13 -0300

    Added a tcp echo server.

---
+++ lib/tests/io_test.cpp
@@ -0,0 +1,29 @@
+#include <thr_queue/util_queue.h>
+#include <aio/aio_tcp.h>
+
+#include <gtest/gtest.h>
+
+using namespace game_engine::aio;
+using namespace game_engine;
+
+TEST(TimeSubsystem, TimeChanges)
+{
+  thr_queue::default_par_queue().submit_work([] {
+    passive_tcp_socket server;
+    auto bind_listen_result_fut = server.bind_and_listen(4000)->perform();
+    auto bind_listen_res = bind_listen_result_fut.get();
+    auto accept_res = server.accept()->perform().get();
+    active_tcp_socket client(std::move(accept_res.client_sock));
+
+    while (true) {
+      auto rres = client.read(1024, 1024 * 1024)->perform().get();
+      if (rres.last_status > 0) {
+        rres.buf.len = rres.already_read;
+        client.write(std::move(rres.buf))->perform();
+      } else if (rres.last_status < 0) {
+        std::cout << "Error: " << uv_strerror(rres.last_status) << '\n';
+        break;
+      }
+    }
+  }).wait();
+}
