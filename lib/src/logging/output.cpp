6826b3592354de5bbf4d70392f47a1b173871e8a
Nicolás Luciano Bértolo <nicolasbertolo@gmail.com>
2015-11-15 01:09:11 -0300

    Changed the log output from stderr to stdout.

--- lib/src/logging/output.cpp
+++ lib/src/logging/output.cpp
@@ -2,19 +2,21 @@
 
 namespace game_engine {
 namespace logging {
-class stderr_output : public output
+class stdout_output : public output
 {
 public:
   void handle_complete_message(std::unique_ptr<message> me) override
   {
-    std::cerr << '[' << me->file << ": " << me->line << "] " << me->ss.str()
+    std::ostringstream ss;
+    ss << '[' << me->file << ": " << me->line << "] " << me->ss.str()
               << std::endl;
+    printf("%s", ss.str().c_str());
   }
 };
 
 void handle_complete_message(std::unique_ptr<message> me)
 {
-  stderr_output output;
+  stdout_output output;
   output.handle_complete_message(std::move(me));
 }
 }
