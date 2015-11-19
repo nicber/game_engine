d73f4e3f08cf59e3691bbaa200000a90697e947f
Nicolas Bertolo <nicolasbertolo@gmail.com>
2015-11-18 23:15:26 -0300

    Forgot to add this file to the commit that changes future.

--- lib/src/thr_queue/event/future.cpp
+++ lib/src/thr_queue/event/future.cpp
@@ -5,23 +5,52 @@
 namespace game_engine {
 namespace thr_queue {
 namespace event {
+void
+future_promise_priv_shared::notify_all_cvs(functor_ptr func)
+{
+  auto notify_work = [d = shared_from_this(), func = std::move(func)] {
+    boost::unique_lock<mutex> l(d->mt);
+    (*func)();
+    d->set_flag = true;
+    d->cv.notify();
+    for (auto it = d->when_any_callbacks.begin(); it != d->when_any_callbacks.end();) {
+      std::shared_ptr<condition_variable> cv_ptr(*it);
+      if (!cv_ptr) {
+        it = d->when_any_callbacks.erase(it);
+      } else {
+        cv_ptr->notify();
+        ++it;
+      }
+    }
+  };
+
+  if (!running_coroutine_or_yielded_from) {
+    default_par_queue().submit_work(std::move(notify_work)).wait();
+  } else {
+    notify_work();
+  }
+}
+
 void promise<void>::set_value()
 {
-  boost::lock_guard<boost::mutex> l(d->os_mt);
-  if (d->set_flag) {
-    std::ostringstream ss;
-    ss << "attempting to set an already set promise<void>";
-    LOG() << ss.str();
-    throw promise_already_set(ss.str());
+  std::exception_ptr except_ptr = nullptr;
+  this->d->notify_all_cvs(make_functor([&except_ptr, d = this->d] {
+    if (d->set_flag) {
+       except_ptr = std::make_exception_ptr(
+                    promise_already_set("attempting to set an already set promise<void>"));
+       return;
+    }
+  }));
+  if (except_ptr) {
+    std::rethrow_exception(except_ptr);
   }
-  notify_all_cvs();
 }
 
 void future_generic_base::wait() const
 {
   if (running_coroutine_or_yielded_from) {
     auto &d = get_priv();
-    boost::unique_lock<mutex> l(d.co_mt);
+    boost::unique_lock<mutex> l(d.mt);
     if (d.set_flag) {
       return;
     } else {
@@ -43,7 +72,7 @@ void future_generic_base::wait() const
 bool future_generic_base::ready() const
 {
   auto &d = get_priv();
-  boost::lock_guard<boost::mutex> l(d.os_mt);
+  boost::lock_guard<mutex> l(d.mt);
   return d.set_flag;
 }
 }
