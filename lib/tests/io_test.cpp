f2a05209dcddcaa47ba87ad5c5f1996b60d6127a
Nicolas Bertolo <nicolasbertolo@gmail.com>
2015-11-19 14:53:09 -0300

    Added a lot of logging to the EchoServer test. It is disabled by default.

--- lib/tests/io_test.cpp
+++ lib/tests/io_test.cpp
@@ -8,51 +8,49 @@
 using namespace game_engine::aio;
 using namespace game_engine;
 
-static bool got_accept_res = false;
-static unsigned int entered_read_loop = 0;
-
 TEST(AIOSubsystem, EchoServer)
 {
-  //game_engine::logging::set_default_for_all(game_engine::logging::policy::disable);
+  game_engine::logging::set_default_for_file(__FILE__,
+                                             game_engine::logging::policy::disable);
   const uint16_t port = 4000;
   const size_t data_transmit = 1024 * 2; // 1 MB
   aio_buffer sent_data;
   aio_buffer received_data;
   thr_queue::event::promise<void> listening;
   auto server_result = thr_queue::default_par_queue().submit_work([&, port, data_transmit] {
-    passive_tcp_socket server;
-    server.bind_and_listen(port)->perform().get();
-    listening.set_value();
-
-
-    LOG() << "Called accept->perform()";
-    auto accept_res_fut = server.accept()->perform();
-    LOG() << "Got accept future";
-    auto accept_res = accept_res_fut.get();
-    LOG() << "Got incomming connection";
-    got_accept_res = true;
-    active_tcp_socket client(std::move(accept_res.client_sock));
-
-    size_t left_to_read = data_transmit;
-    while (left_to_read > 0) {
-      ++entered_read_loop;
-      auto rres = client.read(1, left_to_read)->perform().get();
-      EXPECT_GE(rres.last_status, 0);
-      if (rres.last_status > 0) {
-        left_to_read -= rres.already_read;
-        rres.buf.len = rres.already_read;
-        received_data.append(std::move(rres.buf));
-      } else if (rres.last_status < 0) {
-        std::ostringstream ss;
-        ss << "Error reading: " << uv_strerror(rres.last_status);
-        LOG() << ss.str();
-        throw aio_runtime_error(ss.str());
+    {
+      LOG() << "Started server";
+      passive_tcp_socket server;
+      LOG() << "Initialized server";
+      server.bind_and_listen(port)->perform().get();
+      listening.set_value();
+      LOG() << "Called accept->perform()";
+      auto accept_res_fut = server.accept()->perform();
+      LOG() << "Got accept future";
+      auto accept_res = accept_res_fut.get();
+      LOG() << "Got incomming connection";
+      active_tcp_socket client(std::move(accept_res.client_sock));
+      size_t left_to_read = data_transmit;
+      while (left_to_read > 0) {
+        auto rres = client.read(1, left_to_read)->perform().get();
+        EXPECT_GE(rres.last_status, 0);
+        if (rres.last_status > 0) {
+          left_to_read -= rres.already_read;
+          rres.buf.len = rres.already_read;
+          received_data.append(std::move(rres.buf));
+        } else if (rres.last_status < 0) {
+          std::ostringstream ss;
+          ss << "Error reading: " << uv_strerror(rres.last_status);
+          LOG() << ss.str();
+          throw aio_runtime_error(ss.str());
+        }
       }
     }
-    LOG() << received_data.len;
+    LOG() << "Exiting server";
   });
   listening.get_future().wait();
   thr_queue::default_par_queue().submit_work([&, port, data_transmit] {
+    LOG() << "Started client";
     active_tcp_socket client;
     sockaddr_storage addr;
     uv_ip4_addr("127.0.0.1", 4000, (sockaddr_in*)&addr);
@@ -72,7 +70,9 @@ TEST(AIOSubsystem, EchoServer)
       aio_buffer send_buffer(1024);
       std::generate_n(send_buffer.base, send_buffer.len, rand);
       sent_data.append(send_buffer);
+      LOG() << "Sending";
       client.write(std::move(send_buffer));
+      LOG() << "Sent";
       data_left_to_send -= 1024;
     }
   }).wait();
