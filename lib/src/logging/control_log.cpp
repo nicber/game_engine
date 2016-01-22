3a68f1bf17540da93b7b2ad42c2d7a0361f8dc77
Nicolas Bertolo <nicolasbertolo@gmail.com>
2016-01-21 23:46:54 -0300

    Changed default logging policy to 'disabled'.

--- lib/src/logging/control_log.cpp
+++ lib/src/logging/control_log.cpp
@@ -14,7 +14,7 @@ bool enabled(const char *file, unsigned int line)
 }
 
 static std::atomic<bool> iterating(false);
-static policy default_policy = policy::enable;
+static policy default_policy = policy::disable;
 using line_map = std::unordered_map<unsigned int, policy>;
 static std::unordered_map<std::string, line_map> file_line_policies;
 static boost::recursive_mutex policy_mutex;
